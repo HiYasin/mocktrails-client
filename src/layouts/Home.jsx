4e90f7cb9743835c2698d6c8263fa9298f238707
HiYasin <hello.mr.faisal@gmail.com>
2024-12-17 11:47:39 +0600

    Router+DaisyUI installed and initial route setup

---
+++ src/layouts/Home.jsx
@@ -0,0 +1,20 @@
+import Navbar from "../components/Navbar";
+
+
+const Home = () => {
+    return (
+        <>
+            <header className="w-11/12 max-w-screen-xl mx-auto">
+                <Navbar></Navbar>
+            </header>
+            <main>
+                <h1 className="text-center">Home Page</h1>
+            </main>
+            <footer>
+                <h1 className="text-center">This is Footer</h1>
+            </footer>
+        </>
+    );
+};
+
+export default Home;
