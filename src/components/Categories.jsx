f64c786cd6566a254ad581aa790cd3ef827c391e
Nurul Azim Sakib <sakibruns@gmail.com>
2024-12-19 17:15:03 +0600

    Category section completed

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -6,6 +6,12 @@ import {
   FaEnvelope,
   FaStar,
 } from "react-icons/fa"; 
+import { MdOutlineEngineering,MdOutlineDesignServices} from "react-icons/md";
+import { AiOutlineProduct } from "react-icons/ai";
+import { GoDatabase } from "react-icons/go";
+import { TbZoomMoney } from "react-icons/tb";
+import { IoBagCheckOutline } from "react-icons/io5";
+
 const CategoriesGrid = () => {
   const [categories, setCategories] = useState([]);
 
@@ -13,34 +19,55 @@ const CategoriesGrid = () => {
     const data = [
       {
         id: 1,
-        name: "Category 1",
-        icon: <FaSearch className="text-4xl text-gray-800" />,
+        name: "Engineering",
+        icon: <MdOutlineEngineering className="text-4xl"/>,
       },
       {
         id: 2,
-        name: "Category 2 ",
-        icon: <FaSearch className="text-4xl text-gray-800" />,
+        name: "Marketing ",
+        icon: <IoBagCheckOutline className="text-4xl text-gray-800" />,
       },
+     
       {
         id: 3,
-        name: "Category 3",
-        icon: <FaSearch className="text-4xl text-gray-800" />,
+        name: "Product Design",
+        icon: <AiOutlineProduct  className="text-4xl text-gray-800" />,
+      },
+      {
+        id: 4,
+        name: "Design",
+        icon: <MdOutlineDesignServices className="text-4xl text-gray-800" />,
+      },
+      {
+        id: 5,
+        name: "Data science",
+        icon: <GoDatabase className="text-4xl text-gray-800" />,
+      },
+      {
+        id: 6,
+        name: "Sales",
+        icon: <TbZoomMoney className="text-4xl text-gray-800" />,
       },
     ];
     setCategories(data); // Correctly setting the state here
   }, []); // useEffect ensures this runs once on component mount
 
   return (
-    <main className="container w-full md:w-10/12 my-16 mx-auto">
-      <div className="grid grid-cols-12 w-full gap-3">
-        <div className="col-span-12">
-          <h3 className="font-medium text-2xl"> Serach By Categories</h3>
+    <main className="container w-full md:w-10/12 my-24 mx-auto">
+      <div className="grid grid-cols-12 w-full gap-3 justify-center items-center">
+        <div className="col-span-12 md:col-span-3">
+          <h3 className="font-medium text-2xl md:text-4xl"> Serach By Categories</h3>
+          <p className="font-medium text-gray-500 my-3">Find. <br></br>BOOK.<br></br>GET BETTER.</p>
+          <button className="btn btn-sm bg-green-500 rounded-xl w-fit min-h-10">
+          See all
+        </button>
         </div>
+        <div className="col-span-12 md:col-span-9 grid grid-cols-12 gap-2">
         {categories.map((items) => {
           return (
             <div
               key={items.id}
-              className="col-span-6 bg-white p-16 rounded-lg shadow hover:shadow-lg transition duration-300 overflow-hidden"
+              className="col-span-6 md:col-span-3 bg-white p-10  rounded-lg shadow hover:shadow-lg transition duration-300 overflow-hidden"
             >
             <div className="flex flex-col items-center text-center">
             <div>{items.icon}</div>
@@ -52,6 +79,8 @@ const CategoriesGrid = () => {
             </div>
           );
         })}
+        </div>
+
       </div>
     </main>
   );
--- src/components/OfferingsGrid.jsx
+++ src/components/OfferingsGrid.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { FaUserTie, FaSearch, FaVideo, FaEnvelope, FaStar } from "react-icons/fa"; // Sample icons
-
+import { FaUserTie, FaSearch, FaVideo, FaEnvelope, FaStar,FaRegCommentAlt } from "react-icons/fa"; // Sample icons
+import { LuUserRoundCog,LuUserRoundSearch, LuUserSearch,LuStar,LuVideo} from "react-icons/lu";
 const OfferingsGrid = () => {
   const [offerings, setOfferings] = useState([]);
 
@@ -9,56 +9,56 @@ const OfferingsGrid = () => {
       {
         id: 1,
         heading: "Profile Creation",
-        icon: <FaUserTie className="text-4xl text-gray-800" />,
+        icon: <LuUserRoundCog className="text-4xl text-gray-800" />,
         details:
           "Users can create detailed profiles highlighting their professional background, skills, and areas of expertise. This information helps facilitate better mentor-mentee matches.",
       },
       {
         id: 2,
         heading: "Mentor Search",
-        icon: <FaSearch className="text-4xl text-gray-800" />,
+        icon: <LuUserSearch className="text-4xl text-gray-800" />,
         details:
           "Users can search for mentors based on specific criteria such as industry, location, language, and availability. Advanced filters help narrow down search results to find the most suitable mentors.",
       },
       {
         id: 3,
         heading: "Video Conferencing",
-        icon: <FaVideo className="text-4xl text-gray-800" />,
+        icon: <LuVideo className="text-4xl text-gray-800" />,
         details:
           "The platform offers video conferencing capabilities, allowing mentees and mentors to schedule and conduct mentoring sessions remotely. This feature provides flexibility and convenience for users regardless of their location.",
       },
       {
         id: 4,
         heading: "Messaging System",
-        icon: <FaEnvelope className="text-4xl text-gray-800" />,
+        icon: <FaRegCommentAlt className="text-4xl text-gray-800" />,
         details:
           "This platform provides a built-in messaging system that allows mentees to communicate with potential mentors before initiating a mentoring relationship. This feature enables mentees to ask questions, discuss goals, and gauge compatibility with mentors.",
       },
       {
         id: 5,
         heading: "Feedback & Ratings",
-        icon: <FaStar className="text-4xl text-gray-800" />,
+        icon: <LuStar className="text-4xl text-gray-800" />,
         details:
           "After each mentoring session, both mentees and mentors can provide feedback and ratings based on their experience. This feature helps maintain the quality of mentoring relationships and allows users to continuously improve.",
       },
       {
         id: 6,
         heading: "Feedback & Ratings",
-        icon: <FaStar className="text-4xl text-gray-800" />,
+        icon: <LuStar className="text-4xl text-gray-800" />,
         details:
           "After each mentoring session, both mentees and mentors can provide feedback and ratings based on their experience. This feature helps maintain the quality of mentoring relationships and allows users to continuously improve.",
       },
       {
         id: 7,
         heading: "Messaging System",
-        icon: <FaEnvelope className="text-4xl text-gray-800" />,
+        icon: <FaRegCommentAlt className="text-4xl text-gray-800" />,
         details:
           "This platform provides a built-in messaging system that allows mentees to communicate with potential mentors before initiating a mentoring relationship. This feature enables mentees to ask questions, discuss goals, and gauge compatibility with mentors.",
       },
       {
         id: 8,
         heading: "Video Conferencing",
-        icon: <FaVideo className="text-4xl text-gray-800" />,
+        icon: <LuVideo className="text-4xl text-gray-800" />,
         details:
           "The platform offers video conferencing capabilities, allowing mentees and mentors to schedule and conduct mentoring sessions remotely. This feature provides flexibility and convenience for users regardless of their location.",
       },
@@ -69,11 +69,11 @@ const OfferingsGrid = () => {
   return (
     <div className="w-full md:w-10/12 container mx-auto">
       <h1 className="text-3xl font-bold text-center mb-6">We ARE OFFERING</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid my-16 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-0">
         {offerings.map((item) => (
           <div
             key={item.id}
-            className="relative group w-full bg-white p-16 rounded-lg shadow hover:shadow-lg transition duration-300 overflow-hidden"
+            className="relative group w-full bg-white p-16 border-r-2 border-b-2 border-gray-200 hover: transition duration-300 overflow-hidden"
           >
             <div className="flex flex-col items-center text-center group-hover:opacity-0 group-hover:invisible transition-opacity duration-300">
               {item.icon}
